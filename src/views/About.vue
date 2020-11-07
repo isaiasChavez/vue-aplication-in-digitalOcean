9d3ce8f6c37fa066e64e5e1b0fa44de164f696cb
Isaias <isaiaschavez.co@gmail.com>
2020-11-07 16:34:33 -0600

    First commit

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+const URI = "https://jsonplaceholder.typicode.com/posts";
+
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      const response = await fetch(URI);
+      const data = await response.json();
+      this.posts = data;
+    },
+  },
+};
+</script>
